22a5d2466588e35da7ab9294ec7a9f4dc9442c10
yevhensydorov <assid@ukr.net>
2018-03-22 17:36:29 +0000

    add first api route

--- BE/routers/router.js
+++ BE/routers/router.js
@@ -1,6 +1,22 @@
 const router = require('express').Router();
+const pgp = require('pg-promise')();
+
+
+const db = pgp({
+	host: 'localhost',
+	port: 5432,
+	database: process.env.DATABASE,
+	user: process.env.USERNAME,
+	password: process.env.PASSWORD
+});
+
 
 //ROUTES GO HERE
+router.get('/resources', (req, res) => {
+	db.any(`SELECT * FROM resources`)
+		.then(data => res.json(data))
+		.catch(error => res.json({error: error.message}))
+})
 
 router.get('/test', (req, res) => {
 	res.send("Hello from test")
