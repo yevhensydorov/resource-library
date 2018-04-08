2ba84584e7ff604372f9eb688d6ff869c9e42ecd
coderyourfuture <you@example.com>
2018-04-08 02:20:57 +0100

    Searching field enabled

--- src/Resources.js
+++ src/Resources.js
@@ -2,23 +2,30 @@ import React from 'react';
 import ResourceItem from './ResourceItem';
 
 class Resources extends React.Component {
-	render() {
-		const resources = this.props.resources;
-		let resourcesComponentsArray = resources.map((resourceInfo, index) => {
-			return <ResourceItem 
-				key={index}
-				title={resourceInfo.title}
-				description={resourceInfo.description}
-				url={resourceInfo.url}
-			/>;
-		});
-		return (
-			<section className="resources">
-				<h2>Resources</h2>
-				{resourcesComponentsArray}
-			</section>
-		);
-	}
+  render() {
+    const { resources } = this.props;
+    const filteredResources = resources.filter(Title =>
+      Title.title.toLowerCase().includes(this.props.search)
+    );
+    let resourcesComponentsArray = filteredResources.map(
+      (resourceInfo, index) => {
+        return (
+          <ResourceItem
+            key={index}
+            title={resourceInfo.title}
+            description={resourceInfo.description}
+            url={resourceInfo.url}
+          />
+        );
+      }
+    );
+    return (
+      <section className="resources">
+        <h2>Resources</h2>
+        {resourcesComponentsArray}
+      </section>
+    );
+  }
 }
 
 export default Resources;
--- src/Search.js
+++ src/Search.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
 class Search extends React.Component {
-	render() {
-		return (
-			<form style={{paddingTop:'12px'}} >
-			<div>
-			  <input 
-			  type="search" 
-			  name="resourceSearch"
-			  placeholder="Search for resouces..." 
-			  size="30"/>
-			  <button type='search' ><i className ="fa fa-search"></i></button>
-			</div>
-		  </form>
-		);
-	}
+  render() {
+    return (
+      <form style={{ paddingTop: '12px' }}>
+        <div>
+          <input
+            type="search"
+            value={this.props.search}
+            onChange={this.props.handleSearch}
+            name="resourceSearch"
+            placeholder="Search for resouces..."
+            size="30"
+          />
+          <button type="search">
+            <i className="fa fa-search" />
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default Search;
