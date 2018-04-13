afe89673577be32ba0e981e0302ecd7afcbc312b
coderyourfuture <you@example.com>
2018-04-13 01:39:44 +0100

    code style changed

--- src/Resources.js
+++ src/Resources.js
@@ -5,28 +5,25 @@ class Resources extends React.Component {
   render() {
     const { resources, search } = this.props;
     let query = search.toLowerCase();
-    const filteredResources = resources.filter(
-      ({ title, description }) =>
-        title.toLowerCase().includes(query) ||
-        description.toLowerCase().includes(query)
-    );
-    const resourcesComponentsArray = filteredResources.map(
-      (resourceInfo, index) => {
-        return (
-          <ResourceItem
-            key={index}
-            title={resourceInfo.title}
-            description={resourceInfo.description}
-            url={resourceInfo.url}
-          />
-        );
-      }
-    );
+    const filteredResources = resources
+      .filter(
+        ({ title, description }) =>
+          title.toLowerCase().includes(query) ||
+          description.toLowerCase().includes(query)
+      )
+      .map((resourceInfo, index) => (
+        <ResourceItem
+          key={index}
+          title={resourceInfo.title}
+          description={resourceInfo.description}
+          url={resourceInfo.url}
+        />
+      ));
 
     return (
       <section className="resources">
         <h2>Resources</h2>
-        {resourcesComponentsArray}
+        {filteredResources}
       </section>
     );
   }
