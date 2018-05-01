10093bac032441729e3c1a03e079729b9444a1fe
coderyourfuture <you@example.com>
2018-05-01 16:51:21 +0100

    unnecessary function removed

--- src/DropDownMenu.js
+++ src/DropDownMenu.js
@@ -4,17 +4,7 @@ export default class DropDownMenu extends React.Component {
   constructor(props) {
     super(props);
 
-    this.toggle = this.toggle.bind(this);
     this.handleClick = this.handleClick.bind(this);
-    this.state = {
-      dropdownOpen: false
-    };
-  }
-
-  toggle() {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen
-    });
   }
 
   handleClick(e) {
@@ -27,14 +17,16 @@ export default class DropDownMenu extends React.Component {
   }
 
   render() {
-    let message = "You selected " + this.props.select;
     return (
       <div>
-        <select className="drop-down" value={this.props.select} onChange={this.handleClick}>
+        <select
+          className="drop-down"
+          value={this.props.select}
+          onChange={this.handleClick}
+        >
           <option value="popular">popular</option>
           <option value="alphabetical">Alphabetical</option>
         </select>
-        <p>{message}</p>
       </div>
     );
   }
